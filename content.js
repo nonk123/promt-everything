8af3395bde15dca8789779a7afc00c6202590779
nonk123 <me@nonk.dev>
2026-02-11 20:43:30 +0300

    fix #1 but BREAK EVERYTHING ELSE (needs a better separator!!!)

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const SEPARATOR = "\n\n----";
-const BATCH_SIZE = 3072;
+const BATCH_SIZE = 4096;
 
 Object.defineProperty(String.prototype, "bytes", {
     get: function () {
@@ -7,7 +7,28 @@ Object.defineProperty(String.prototype, "bytes", {
     }
 })
 
-function processChunk(targetLang, nodes, chunk) {
+function processLongString(targetLang, node, remainder, depth) {
+    if (!remainder.length)
+        return;
+
+    const chunk = remainder.bytes > BATCH_SIZE ? remainder.substring(0, BATCH_SIZE) : remainder;
+    remainder = remainder.bytes > BATCH_SIZE ? remainder.substring(BATCH_SIZE) : "";
+
+    const msg = { action: "promt", targetLang, text: chunk };
+    browser.runtime.sendMessage(msg).then(r => {
+        if (!r.success) {
+            console.error(r.error);
+            return;
+        }
+        node.textContent = depth ? r.translation : node.textContent + r.translation;
+        processLongString(targetLang, node, remainder, depth + 1);
+    });
+}
+
+function processBatch(targetLang, nodes, chunk) {
+    if (!nodes.length || !chunk.length)
+        return;
+
     const msg = { action: "promt", targetLang, text: chunk };
     browser.runtime.sendMessage(msg).then(r => {
         if (!r.success) {
@@ -15,7 +36,7 @@ function processChunk(targetLang, nodes, chunk) {
             return;
         }
 
-        const strings = r.translation.split(SEPARATOR);
+        const strings = r.translation.trim().split(SEPARATOR);
         while (nodes.length) {
             const node = nodes.shift();
             const text = strings.shift();
@@ -44,17 +65,28 @@ function promtEverything() {
 
     while (walker.nextNode()) {
         const node = walker.currentNode;
-        const txt = node.textContent;
-        if (!txt.length)
+        const text = node.textContent;
+
+        if (!text.length)
             continue;
-        if (chunk.bytes + txt.bytes + SEPARATOR.bytes >= BATCH_SIZE) {
-            processChunk(targetLang, nodes, chunk);
+
+        const long = text.bytes >= BATCH_SIZE;
+        const batchFull = chunk.bytes + text.bytes + SEPARATOR.bytes >= BATCH_SIZE;
+
+        if (long || batchFull) {
+            processBatch(targetLang, nodes, chunk);
             nodes = [], chunk = "";
         }
-        nodes.push(node);
-        chunk += txt + SEPARATOR;
+
+        if (long) {
+            processLongString(targetLang, node, text, 0)
+        } else {
+            nodes.push(node);
+            chunk += text + SEPARATOR;
+        }
     }
-    processChunk(targetLang, nodes, chunk);
+
+    processBatch(targetLang, nodes, chunk);
 }
 
 if (typeof browser === "undefined")
