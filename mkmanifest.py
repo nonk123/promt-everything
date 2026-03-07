4502ae5436d8c243944cd3186dec3344a00f251a
nonk123 <me@nonk.dev>
2026-03-07 11:49:13 +0300

    nitpick at the manifest generator + format it with black

--- mkmanifest.py
+++ mkmanifest.py
@@ -4,7 +4,9 @@ import json
 import sys
 
 NAME = "ProMT Everything"
-DESCRIPTION = "Adds a button that feeds your whole tab into ProMT through an external public API."
+DESCRIPTION = (
+    "Adds a button that feeds your whole tab into ProMT through an external public API."
+)
 VERSION = "0.1.0"
 
 GECKO_ID = "promt-everything@nonk.dev"
@@ -13,7 +15,7 @@ ICONS = {
     "16": "icons/16.png",
     "24": "icons/24.png",
     "32": "icons/32.png",
-    "48": "icons/48.png"
+    "48": "icons/48.png",
 }
 
 PERMISSIONS = ["activeTab"]
@@ -27,55 +29,55 @@ ACTION = {
 WORKER_SCRIPT = "background.js"
 CONTENT_SCRIPTS = [
     {
-        "matches": [
-            "<all_urls>"
-        ],
-        "js": [
-            "content.js"
-        ]
+        "matches": ["<all_urls>"],
+        "js": ["content.js"],
     }
 ]
 
-generator = sys.argv[1]
-if generator != "firefox" and generator != "chrome":
-    print("meh")
-    sys.exit(1)
 
-browser_specifics = {} if generator == "chrome" else {
-    "browser_specific_settings": {
-        "gecko": {
-            "id": GECKO_ID,
-            "data_collection_permissions": {
-                "required": ["none"]
+def expect(value):
+    if not value:
+        print("meh")
+        sys.exit(1)
+
+
+def main():
+    expect(len(sys.argv) == 2)
+
+    generator = sys.argv[1]
+    expect(generator == "firefox" or generator == "chrome")
+
+    if generator == "firefox":
+        background = {"scripts": [WORKER_SCRIPT]}
+        browser_specifics = {
+            "browser_specific_settings": {
+                "gecko": {
+                    "id": GECKO_ID,
+                    "data_collection_permissions": {"required": ["none"]},
+                },
             }
-        },
+        }
+    else:
+        background = {"service_worker": WORKER_SCRIPT}
+        browser_specifics = {}
+
+    manifest = {
+        "manifest_version": 3,
+        "name": NAME,
+        "version": VERSION,
+        "description": DESCRIPTION,
+        "icons": ICONS,
+        "permissions": PERMISSIONS,
+        "host_permissions": HOST_PERMISSIONS,
+        "action": ACTION,
+        "content_scripts": CONTENT_SCRIPTS,
+        "background": background,
+        **browser_specifics,
     }
-}
 
-background = {
-    "background": {
-        "scripts": [WORKER_SCRIPT],
-    }
-} if generator == "firefox" else {
-    "background": {
-        "service_worker": WORKER_SCRIPT
-    }
-}
+    with open("manifest.json", "w") as f:
+        json.dump(manifest, f, indent=4)
 
-manifest = {
-    "manifest_version": 3,
-    "name": NAME,
-    "version": VERSION,
-    "description": DESCRIPTION,
-    "icons": ICONS,
-    "permissions": PERMISSIONS,
-    "host_permissions": HOST_PERMISSIONS,
-    "action": ACTION,
-    "content_scripts": CONTENT_SCRIPTS,
-}
 
-manifest.update(browser_specifics)
-manifest.update(background)
-
-with open("manifest.json", "w") as f:
-    json.dump(manifest, f, indent = 4)
+if __name__ == "__main__":
+    main()
